857b852812fdfb570cc8fad03a81420415a750df
sammileungg <sammileungg@gmail.com>
2013-12-03 16:44:43 -0800

    setup of all files

---
+++ base/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title> BASE </title>
+        <script src="jquery.js"></script>
+        <script type="text/javascript" src="js_base.js"></script>
+		<link href="css_base.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+	</body>
+</html>
---
+++ unlocked/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title> UNLOCKED </title>
+        <script src="jquery.js"></script>
+        <script type="text/javascript" src="js_concert.js"></script>
+		<link href="css_concert.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+	</body>
+</html>
