e5737d35e7211baf02941f77bc7980db718e230d
Aslan Law <aslanlaw15@gmail.com>
2013-12-04 18:44:19 -0800

    concert page + css

--- concert/index.php
+++ concert/index.php
@@ -7,11 +7,47 @@
         <script type="text/javascript" src="js_concert.js"></script>
 		<link href="css_concert.css" rel="stylesheet" type="text/css" />
 	</head>
-	<body style="background-color:black;">
-		<div class="main-bg">
-			<div class="black-overlay">
-				
+	<body>
+
+		<!-- header -->
+		<div id="header_container">
+			<div id="header">
+				<div class="zippo-signin pull-right">
+					<form>
+						<input class="input-small" type="text" required="required" name="zippo_id" placeholder="Zippo ID" />
+						<button class="btn" type="submit">Enter</button>
+					</form>
+				</div>
 			</div>
 		</div>
+		<!-- end header -->
+
+		<!-- body -->
+		<div class="body_container">
+			<div class="main-bg">
+				<div class="black-overlay">
+					<p>Please check back after the Concert!</p>
+				</div>
+			</div>
+		</div>
+		<!-- end body -->
+
+		<!-- footer -->
+		<div id="footer_container">
+			<div id="footer">
+				<div id="footer_nav">
+					<div class="about">
+						<p>About</p>
+					</div>
+					<div class="zippo_web">
+						<p>Zippo</p>
+					</div>
+					<div class="social_share pull-right">
+						<p>Share</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end footer -->
 	</body>
 </html>
