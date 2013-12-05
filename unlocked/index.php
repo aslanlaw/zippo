eee312fc01c7dbb0afed943315c76d446e484669
Aslan Law <aslanlaw15@gmail.com>
2013-12-05 00:30:03 -0800

    button test

--- unlocked/index.php
+++ unlocked/index.php
@@ -2,11 +2,65 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> UNLOCKED </title>
+        <title> LOCKED </title>
         <script src="jquery.js"></script>
-        <script type="text/javascript" src="js_unlocked.js"></script>
+        <script type="text/javascript" src="js_locked.js"></script>
 		<link href="css_unlocked.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
+		<!-- header -->
+		<div id="header_container">
+			<div id="header">
+				<div class="zippo-signin pull-right">
+					<form>
+						<input class="input-small" type="text" required="required" name="zippo_id" placeholder="Zippo ID" />
+						<button class="btn" type="submit">Enter</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- end header -->
+
+		<!-- body -->
+		<div class="body_container">
+			
+			<div class="main-bg">
+				<!-- BACKGROUND IMG -->
+			</div>
+			
+			<div class="black-overlay">
+				<div class="overlay-about">
+					<p>About</p>
+				</div>
+				<div class="overlay-share">
+					<p>Share</p>
+
+					<!-- SOCIAL MEDIA BUTTONS -->
+
+				</div>
+			</div>
+		
+		</div>
+		<!-- end body -->
+
+		<!-- footer -->
+		<div id="footer_container">
+			<div id="footer">
+				<div id="footer_nav">
+					<div class="about">
+						<a href="#">
+						<img src="img/about_1.png" class="about_button" />
+						</a>
+					</div>
+					<div class="zippo_web">
+						<p>Zippo</p>
+					</div>
+					<div class="social_share pull-right">
+						<p>Share</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end footer -->
 	</body>
 </html>
