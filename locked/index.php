d14c3de0dd09104ce27b443f7a5457bc39010b42
sammileungg <sammileungg@gmail.com>
2013-12-05 03:46:37 -0800

    incorporated about and share clickable in both locked and unlocked

--- locked/index.php
+++ locked/index.php
@@ -13,7 +13,7 @@
 			<div id="header">
 				<div class="zippo-signin pull-right">
 					<a href="#"><img src="img/enter_1.png" class="enter_button" /></a>
-					<form>
+					<form class="zippo_input">
 						<input class="input-small" type="text" required="required" name="zippo_id" placeholder=" #ZippoID" />
 					</form>
 				</div>
@@ -26,16 +26,26 @@
 			
 			<div class="main-bg">
 				<!-- BACKGROUND IMG -->
+				<img src="img/background2.jpg" class="bg_image" />
 			</div>
 			
 			<div id="overlay-about">
 				<div id="content-about">
-					<h1>About</p>
+					<h1>About</h1>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut est viverra, placerat magna eget, egestas massa. Nunc faucibus euismod convallis. Aenean eget ultricies nunc, non tincidunt purus. Fusce nisl dolor, gravida laoreet volutpat nec, ullamcorper vel sapien. Donec eget tortor pharetra ante sodales pulvinar at vel dui. Vestibulum porttitor enim ac orci varius, ac viverra nibh luctus. Aliquam ut nisl eu urna pretium ornare eu a arcu. Vestibulum ultrices, elit eu accumsan rutrum, lacus justo eleifend arcu, at gravida metus nisl in urna. Vestibulum malesuada dui ut eleifend pulvinar. Suspendisse dignissim ultricies ipsum at fringilla. Aliquam pretium viverra pretium. Curabitur ultricies lacus et volutpat tincidunt. In hac habitasse platea dictumst. Aenean sed ornare odio. Donec lacinia nec ante congue porta.
+					</p>
+					<p>
+					Suspendisse sed velit interdum magna sodales eleifend a in ipsum. Maecenas vulputate gravida felis id ultricies. Proin pulvinar volutpat velit congue interdum. Aliquam sit amet ante leo. Sed ac leo in magna fringilla blandit. Sed ut mi sapien. Nullam a interdum arcu.
+					</p>
+					<p>
+					In imperdiet sem id arcu cursus, vel convallis felis convallis. Phasellus imperdiet sed ante eu dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut leo eu tellus lacinia porta vel at orci. In consectetur tellus sed lacinia luctus. Nulla facilisi. Proin faucibus molestie mauris sit amet varius. Vivamus commodo rutrum vestibulum. Aliquam iaculis venenatis risus vel lobortis. Integer sit amet luctus libero. Cras vitae enim auctor, scelerisque augue ac, semper elit. Proin porta tincidunt diam congue convallis. Curabitur porta diam et mauris molestie, non scelerisque odio ultricies.
+					</p>
 				</div>
 			</div>
 			<div id="overlay-share">
 				<div id="content-share">
-					<h1>Share</p>
+					<h1>Share</h1>
 					<!-- SOCIAL MEDIA BUTTONS -->
 				</div>
 			</div>
--- unlocked/index.php
+++ unlocked/index.php
@@ -2,9 +2,9 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> LOCKED </title>
+        <title> UNLOCKED </title>
         <script src="jquery.js"></script>
-        <script type="text/javascript" src="js_locked.js"></script>
+        <script type="text/javascript" src="js_unlocked.js"></script>
 		<link href="css_unlocked.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
@@ -29,15 +29,24 @@
 				<!-- BACKGROUND IMG -->
 			</div>
 			
-			<div class="black-overlay">
-				<div class="overlay-about">
-					<p>About</p>
+			<div id="overlay-about">
+				<div id="content-about">
+					<h1>About</h1>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut est viverra, placerat magna eget, egestas massa. Nunc faucibus euismod convallis. Aenean eget ultricies nunc, non tincidunt purus. Fusce nisl dolor, gravida laoreet volutpat nec, ullamcorper vel sapien. Donec eget tortor pharetra ante sodales pulvinar at vel dui. Vestibulum porttitor enim ac orci varius, ac viverra nibh luctus. Aliquam ut nisl eu urna pretium ornare eu a arcu. Vestibulum ultrices, elit eu accumsan rutrum, lacus justo eleifend arcu, at gravida metus nisl in urna. Vestibulum malesuada dui ut eleifend pulvinar. Suspendisse dignissim ultricies ipsum at fringilla. Aliquam pretium viverra pretium. Curabitur ultricies lacus et volutpat tincidunt. In hac habitasse platea dictumst. Aenean sed ornare odio. Donec lacinia nec ante congue porta.
+					</p>
+					<p>
+					Suspendisse sed velit interdum magna sodales eleifend a in ipsum. Maecenas vulputate gravida felis id ultricies. Proin pulvinar volutpat velit congue interdum. Aliquam sit amet ante leo. Sed ac leo in magna fringilla blandit. Sed ut mi sapien. Nullam a interdum arcu.
+					</p>
+					<p>
+					In imperdiet sem id arcu cursus, vel convallis felis convallis. Phasellus imperdiet sed ante eu dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut leo eu tellus lacinia porta vel at orci. In consectetur tellus sed lacinia luctus. Nulla facilisi. Proin faucibus molestie mauris sit amet varius. Vivamus commodo rutrum vestibulum. Aliquam iaculis venenatis risus vel lobortis. Integer sit amet luctus libero. Cras vitae enim auctor, scelerisque augue ac, semper elit. Proin porta tincidunt diam congue convallis. Curabitur porta diam et mauris molestie, non scelerisque odio ultricies.
+					</p>
 				</div>
-				<div class="overlay-share">
-					<p>Share</p>
-
+			</div>
+			<div id="overlay-share">
+				<div id="content-share">
+					<h1>Share</h1>
 					<!-- SOCIAL MEDIA BUTTONS -->
-
 				</div>
 			</div>
 		
